bbd039483ee29953f5cb8ba76359135a4c61a08e
Jbeach32 <joshbeach2007@gmail.com>
2023-11-02 20:17:12 -0600

    mongo connection

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const PORT = process.env.PORT || 3000; 
+
+mongoose.connect('your-mongodb-connection-string', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+  db.once('open', () => {
+    console.log('Connected to the database');
+  });
+
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
