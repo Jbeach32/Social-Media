55f994c1e8ee02835b86efa813c1650ac3adef4a
Jbeach32 <joshbeach2007@gmail.com>
2023-11-13 16:12:30 -0700

    Friend routes added

--- routes/friend-routes.js
+++ routes/friend-routes.js
@@ -38,6 +38,39 @@ router.post('/api/users/add-friend', async (req, res) => {
   }
 });
 
+// Delete Friends
+router.delete('/api/users/delete-friend', async (req, res) => {
+    try {
+      const { requesterUsername, friendUsername } = req.body;
+  
+      // Find the user with the requester's username
+      const requesterUser = await User.findOne({ username: requesterUsername });
+  
+      if (!requesterUser) {
+        return res.status(404).json({ message: `User ${requesterUsername} not found` });
+      }
+  
+      // Find the friend user by their username
+      const friendUser = await User.findOne({ username: friendUsername });
+  
+      if (!friendUser) {
+        return res.status(404).json({ message: `User ${friendUsername} not found` });
+      }
+  
+      // Remove the friend's ObjectId from the requester's friends array
+      await User.updateOne(
+        { username: requesterUsername },
+        { $pull: { friends: friendUser._id } }
+      );
+  
+      res.json({ message: `Friend ${friendUsername} removed successfully` });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+  
+
 module.exports = router;
 
 
